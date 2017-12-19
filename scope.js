ed88954211528f2281e8f03b7e387bc9d071bff0
tbarf <30032737+tbarf@users.noreply.github.com>
2017-12-18 21:32:30 -1000

    formated

--- scope.js
+++ scope.js
@@ -1,66 +1,59 @@
 // Fill in the closeLid and openLid functions to set the isCookieJarOpen variable to false or true respectively.
-var isCookieJarOpen = null;
+const isCookieJarOpen = null;
 
-function closeLid() {
+function closeLid () {
   /* answer here */
 }
 
-function openLid() {
+function openLid () {
   /* answer here */
 }
-
 
 // Fill in the return value for the inner function to return a string, 'Hello World' using only the variables provided
 // in both inner and outer functions
-function outerFunction() {
-  var hello = 'Hello';
+function outerFunction () {
+  const hello = 'Hello';
 
-  function innerFunction() {
-    var world = 'World';
-    return /* answer here */;
+  function innerFunction () {
+    const world = 'World';
   }
   return innerFunction();
 }
 
-
 // This is a function that takes in a 2d-array (or matrix) and returns the sum of all elements
 // It's broken due to count variables colliding into each other.  Fix it!
-function addMatrixElements(matrix) {
+function addMatrixElements (matrix) {
+  let result = 0;
 
-  var result = 0;
-
-  for(var i = 0; i < matrix.length; i++) {
+  for (var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for (var i = 0; i < matrix[i].length; i++) {
+      result += matrix[i][i];
     }
   }
   return result;
 }
 
-
 // This function is returning the wrong userObject data. It should be returning
 // Neo's information and not Morpheus'.  Fix it!
-function sendDataToClient() {
-
-  var userObject = {
+function sendDataToClient () {
+  const userObject = {
     handle: 'neo',
     authenticated: false
-  }
+  };
 
-  function authenticateUser(obj, username) {
-    var userObject = {
+  function authenticateUser (obj, username) {
+    const userObject = {
       handle: 'morpheus',
       authenticated: false
     };
 
     if (userObject.handle === username) {
       userObject.authenticated = true;
-      return userObject
-    } else {
-      return userObject
+      return userObject;
     }
+    return userObject;
   }
-  authenticateUser(userObject, 'neo')
-  return userObject
+  authenticateUser(userObject, 'neo');
+  return userObject;
 }
